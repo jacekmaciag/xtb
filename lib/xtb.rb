945456a5f58d702fe665780f84645e16638a7e18
jacekmaciag <jacek.maciag@gmail.com>
2024-10-13 15:50:14 +0200

    Initialize gem repo

---
+++ lib/xtb.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "xtb/version"
+
+module Xtb
+  class Error < StandardError; end
+end
---
+++ lib/xtb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Xtb
+  VERSION = "0.1.0"
+end
